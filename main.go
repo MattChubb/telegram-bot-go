ef69fb41953e958cba741a6a90b62ced7585fcee
Matthew Chubb <chubb.matthew@gmail.com>
2020-08-23 15:50:03 +0100

    Add ability to generate sentences

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
     "strings"
 )
 
+const tokensLengthLimit = 32
+
 func main() {
     //Initilise Telegram bot
 
@@ -17,10 +19,11 @@ func main() {
 
     //Train markov chain
     chain.Add(processString("test data test data test data"))
+    chain.Add(processString("data test data test data"))
     chain.Add(processString("test data test data test data"))
-    chain.Add(processString("test data test data test data"))
-    next, _ := chain.Generate([]string{"test", "data", "data"})
-    fmt.Println(next)
+
+    sentence := generateSentence(chain, []string{"test"})
+    fmt.Println(sentence)
 
     //Connect to telegram
 }
@@ -28,4 +31,17 @@ func main() {
 func processString(rawString string) []string {
     //TODO Handle punctuation other than spaces
     return strings.Split(rawString, " ")
+}
+
+func generateSentence(chain *gomarkov.Chain, init []string) []string {
+    tokens := []string{gomarkov.StartToken}
+    tokens = append(tokens, init...)
+
+    for tokens[len(tokens) - 1] != gomarkov.EndToken &&
+        len(tokens) < tokensLengthLimit {
+        next, _ := chain.Generate(tokens[(len(tokens) - 1):] )
+        tokens = append(tokens, next)
+    }
+
+    return tokens
 }
--- main_test.go
+++ main_test.go
@@ -1,26 +1,69 @@
 package main
 
 import (
+    "github.com/mb-14/gomarkov"
     "reflect"
     "testing"
 )
 
 func TestProcessString(t *testing.T) {
     tables := []struct{
+        testcase string
         input string
         expected []string
     }{
-        {"test", []string{"test"}},
-        {"test data", []string{"test", "data"}},
-        {"test data one", []string{"test", "data", "one"}},
-        {"", []string{""}},
-        {"test1data", []string{"test1data"}},
+        {"1 word", "test", []string{"test"}},
+        {"2 words", "test data", []string{"test", "data"}},
+        {"3 words", "test data one", []string{"test", "data", "one"}},
+        {"0 words", "", []string{""}},
+        {"alphanumeric", "test1data", []string{"test1data"}},
     }
 
     for _, table := range tables {
+        t.Logf("Testing: %s", table.testcase)
         got := processString(table.input)
         if ! reflect.DeepEqual(got, table.expected) {
             t.Errorf("expected: %#v, got: %#v", table.expected, got)
+        } else {
+            t.Log("Passed\n")
+        }
+    }
+}
+
+
+func TestGenerateSentence(t *testing.T) {
+    /*
+    Markov chains are inherently random, and so there's no real point in trying to test them deterministically.
+    Instead, we can feed it various prompts and check we get _something_ out the other side
+    TODO Check that what we get out is expected, i.e. contains only the words "test and "data" in that order
+    */
+    tables := []struct{
+        testcase string
+        input []string
+    }{
+        {"Null", []string{}},
+        {"Empty string", []string{""}},
+        {"1 word", []string{"test"}},
+        {"1 word 2", []string{"data"}},
+        {"2 words", []string{"test", "data"}},
+        {"3 words", []string{"test", "data", "test"}},
+        {"Unknown word", []string{"testing"}},
+    }
+
+    chain := gomarkov.NewChain(1)
+
+    chain.Add(processString("test data test data test data"))
+    chain.Add(processString("data test data test data"))
+    chain.Add(processString("test data test data test data"))
+
+    for _, table := range tables {
+        t.Logf("Testing: %s", table.testcase)
+        got := generateSentence(chain, table.input)
+
+        if len(got) < 2 {
+            t.Errorf("prompt: %#v, got: %#v", table.input, got)
+        } else {
+            t.Log("Passed\n")
         }
     }
 }
