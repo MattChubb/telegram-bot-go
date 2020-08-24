c15d68e322dbf3b23c44537363828df7d1b4f208
Matthew Chubb <chubb.matthew@gmail.com>
2020-08-24 21:31:38 +0100

    Code tidying

--- main.go
+++ main.go
@@ -1,125 +1,122 @@
 package main
 
 import (
-    "github.com/tucnak/telebot"
-    "github.com/mb-14/gomarkov"
-    "bufio"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "math/rand"
-    "os"
-    "strings"
-    "time"
+	"bufio"
+	"fmt"
+	"github.com/mb-14/gomarkov"
+	"github.com/tucnak/telebot"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
 )
 
 const (
-    tokensLengthLimit = 32
-    order = 1
-    sourceDir = "./source_data"
+	tokensLengthLimit = 32
+	order             = 1
+	sourceDir         = "./source_data"
 )
 
 func main() {
-    //Initialise
-    //Get Telegram bot token from env
-    bot_token := os.Getenv("TELEGRAM_BOT_TOKEN")
+	//Initialise
+	rand.Seed(time.Now().Unix())
 
-    //Initilise Telegram bot
-    bot, err := telebot.NewBot(telebot.Settings{
-        URL: "",
-        Token: bot_token,
-        Poller: &telebot.LongPoller{
-            Timeout: 10 * time.Second,
-        },
-    })
-    if err != nil {
-        log.Fatal(err)
-    }
+	//Initilise Telegram bot
+	fmt.Println("Initialising bot...")
+	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
+	bot, err := telebot.NewBot(telebot.Settings{
+		URL:   "",
+		Token: botToken,
+		Poller: &telebot.LongPoller{
+			Timeout: 10 * time.Second,
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    //Initialise chain
-    chain := gomarkov.NewChain(order)
+	//Initialise chain
+	fmt.Println("Initialising chain...")
+	chain := gomarkov.NewChain(order)
 
-    //Misc init
-    rand.Seed(time.Now().Unix())
+	//Train
+	fmt.Println("Opening source data...")
+	source_files, err := ioutil.ReadDir(sourceDir)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    //Train
-    source_files, err := ioutil.ReadDir(sourceDir)
-    if err != nil {
-        log.Fatal(err)
-    }
+	fmt.Println("Training chain on source data...")
+	for _, fileInfo := range source_files {
+		if fileInfo.Name()[1] == '.' {
+			continue
+		}
+		sourceFile, err := os.Open(sourceDir + "/" + fileInfo.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+		trainFromFile(chain, sourceFile)
+	}
 
-    for _, fileInfo := range source_files {
-        if fileInfo.Name()[1] == '.' {
-            continue
-        }
-        sourceFile, err := os.Open(sourceDir + "/" + fileInfo.Name())
-        if err != nil {
-            log.Fatal(err)
-        }
-        trainFromFile(chain, sourceFile)
-    }
+	//Connect Markov to Telegram
+	fmt.Println("Adding chain to bot...")
+	bot.Handle(telebot.OnText, func(m *telebot.Message) {
+		parsedMessage := processString(m.Text)
 
-    //Connect Markov to Telegram
-    bot.Handle(telebot.OnText, func(m *telebot.Message) {
-        //Process input
-        rawMessage := m.Text
-        parsedMessage := processString(rawMessage)
+		//Train on input (Ensures we always have a response for new words)
+		chain.Add(parsedMessage)
 
-        //Train on input (Ensures we always have a response for new words)
-        chain.Add(parsedMessage)
+		//TODO Only speak when spoken to
+		respondable := true
+		if !respondable {
+			return
+		}
 
-        //Identify whether to respond
-        respondable := true
-        if !respondable {
-            return
-        }
+		//Generate response
+		subject := append([]string{}, parsedMessage[rand.Intn(len(parsedMessage))])
+		sentence := generateSentence(chain, subject)
+		response := strings.Join(sentence, " ")
 
-        //Identify subject
-        subject := []string{}
-        subject = append(subject, parsedMessage[rand.Intn(len(parsedMessage))])
+		//Respond with generated response
+		bot.Send(m.Sender, response)
+	})
 
-        //Generate response
-        sentence := generateSentence(chain, subject)
-        response := strings.Join(sentence, " ")
-
-        //Respond with generated response
-        bot.Send(m.Sender, response)
-    })
-
-    fmt.Println("Starting bot")
-    bot.Start()
-    fmt.Println("Bot stopped")
+	fmt.Println("Starting bot...")
+	bot.Start()
+	fmt.Println("Bot stopped")
 }
 
 func processString(rawString string) []string {
-    //TODO Handle punctuation other than spaces
-    return strings.Split(rawString, " ")
+	//TODO Handle punctuation other than spaces
+	return strings.Split(rawString, " ")
 }
 
 func generateSentence(chain *gomarkov.Chain, init []string) []string {
-    tokens := []string{gomarkov.StartToken}
-    tokens = append(tokens, init...)
+	tokens := []string{gomarkov.StartToken}
+	tokens = append(tokens, init...)
 
-    for tokens[len(tokens) - 1] != gomarkov.EndToken &&
-        len(tokens) < tokensLengthLimit {
-        next, _ := chain.Generate(tokens[(len(tokens) - 1):] )
-       // fmt.Println(next)
-        if len(next) > 0 {
-            tokens = append(tokens, next)
-        } else {
-            tokens = append(tokens, gomarkov.EndToken)
-        }
-    }
+	for tokens[len(tokens)-1] != gomarkov.EndToken &&
+		len(tokens) < tokensLengthLimit {
+		next, _ := chain.Generate(tokens[(len(tokens) - 1):])
+		// fmt.Println(next)
+		if len(next) > 0 {
+			tokens = append(tokens, next)
+		} else {
+			tokens = append(tokens, gomarkov.EndToken)
+		}
+	}
 
-    return tokens
+	return tokens
 }
 
 func trainFromFile(chain *gomarkov.Chain, file *os.File) {
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        chain.Add(processString(scanner.Text()))
-    }
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		chain.Add(processString(scanner.Text()))
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
--- main_test.go
+++ main_test.go
@@ -1,73 +1,72 @@
 package main
 
 import (
-    "github.com/mb-14/gomarkov"
-    "reflect"
-    "testing"
+	"github.com/mb-14/gomarkov"
+	"reflect"
+	"testing"
 )
 
 func TestProcessString(t *testing.T) {
-    tables := []struct{
-        testcase string
-        input string
-        expected []string
-    }{
-        {"1 word", "test", []string{"test"}},
-        {"2 words", "test data", []string{"test", "data"}},
-        {"3 words", "test data one", []string{"test", "data", "one"}},
-        {"0 words", "", []string{""}},
-        {"alphanumeric", "test1data", []string{"test1data"}},
-        {"punctuation", "test. data,", []string{"test.", "data,"}},
-    }
+	tables := []struct {
+		testcase string
+		input    string
+		expected []string
+	}{
+		{"1 word", "test", []string{"test"}},
+		{"2 words", "test data", []string{"test", "data"}},
+		{"3 words", "test data one", []string{"test", "data", "one"}},
+		{"0 words", "", []string{""}},
+		{"alphanumeric", "test1data", []string{"test1data"}},
+		{"punctuation", "test. data,", []string{"test.", "data,"}},
+	}
 
-    for _, table := range tables {
-        t.Logf("Testing: %s", table.testcase)
-        got := processString(table.input)
-        if ! reflect.DeepEqual(got, table.expected) {
-            t.Errorf("expected: %#v, got: %#v", table.expected, got)
-        } else {
-            t.Log("Passed")
-        }
-    }
+	for _, table := range tables {
+		t.Logf("Testing: %s", table.testcase)
+		got := processString(table.input)
+		if !reflect.DeepEqual(got, table.expected) {
+			t.Errorf("expected: %#v, got: %#v", table.expected, got)
+		} else {
+			t.Log("Passed")
+		}
+	}
 }
 
-
 func TestGenerateSentence(t *testing.T) {
-    /*
-    Markov chains are inherently random, and so there's no real point in trying to test them deterministically.
-    Instead, we can feed it various prompts and check we get _something_ out the other side
-    TODO Check that what we get out is expected:
-    * Contains only the words "test" and "data" in that order
-    * Does not hit the tokensLengthLimit
-    */
-    tables := []struct{
-        testcase string
-        input []string
-    }{
-        {"Null", []string{}},
-        {"Empty string", []string{""}},
-        {"1 word", []string{"test"}},
-        {"1 word 2", []string{"data"}},
-        {"2 words", []string{"test", "data"}},
-        {"3 words", []string{"test", "data", "test"}},
-        {"Unknown word", []string{"testing"}},
-    }
+	/*
+	   Markov chains are inherently random, and so there's no real point in trying to test them deterministically.
+	   Instead, we can feed it various prompts and check we get _something_ out the other side
+	   TODO Check that what we get out is expected:
+	   * Contains only the words "test" and "data" in that order
+	   * Does not hit the tokensLengthLimit
+	*/
+	tables := []struct {
+		testcase string
+		input    []string
+	}{
+		{"Null", []string{}},
+		{"Empty string", []string{""}},
+		{"1 word", []string{"test"}},
+		{"1 word 2", []string{"data"}},
+		{"2 words", []string{"test", "data"}},
+		{"3 words", []string{"test", "data", "test"}},
+		{"Unknown word", []string{"testing"}},
+	}
 
-    chain := gomarkov.NewChain(1)
+	chain := gomarkov.NewChain(1)
 
-    chain.Add(processString("test data test data test data"))
-    chain.Add(processString("data test data test data"))
-    chain.Add(processString("test data test data test data"))
+	chain.Add(processString("test data test data test data"))
+	chain.Add(processString("data test data test data"))
+	chain.Add(processString("test data test data test data"))
 
-    for _, table := range tables {
-        t.Logf("Testing: %s", table.testcase)
-        got := generateSentence(chain, table.input)
+	for _, table := range tables {
+		t.Logf("Testing: %s", table.testcase)
+		got := generateSentence(chain, table.input)
 
-        if len(got) < 2 {
-            t.Errorf("prompt: %#v, got: %#v", table.input, got)
-        } else {
-            //t.Logf("Got: %#v", got)
-            t.Logf("Passed (%d tokens returned)", len(got))
-        }
-    }
+		if len(got) < 2 {
+			t.Errorf("prompt: %#v, got: %#v", table.input, got)
+		} else {
+			//t.Logf("Got: %#v", got)
+			t.Logf("Passed (%d tokens returned)", len(got))
+		}
+	}
 }
