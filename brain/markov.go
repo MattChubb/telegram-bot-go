5fde8ba76c5217b7a4ebe08f37c1f6f9ab8676d6
Matthew Chubb <chubb.matthew@gmail.com>
2020-09-21 18:59:56 +0100

    Ability to train markovbrain object

--- brain/markov.go
+++ brain/markov.go
@@ -2,18 +2,31 @@ package markov
 
 import (
 	"github.com/mb-14/gomarkov"
+	log "github.com/sirupsen/logrus"
+	"strings"
 )
 
 type Brain struct {
-    chain gomarkov.Chain
+    chain *gomarkov.Chain
 }
 
-func (brain Brain) Init(order int) {
-	brain.chain = *gomarkov.NewChain(order)
+func (brain *Brain) Init(order int) {
+	brain.chain = gomarkov.NewChain(order)
+    log.Debug("Braindump: ", brain)
 }
 
-func (brain Brain) Train(data string) error {
+func (brain *Brain) Train(data string) error {
+    log.Debug("Braindump: ", brain)
+    log.Debug("Training data: ", data)
+    processedData := processString(data)
+    log.Debug("Processed into: ", processedData)
+    brain.chain.Add(processedData)
     return nil
 }
 
 func Generate() {}
+
+func processString(rawString string) []string {
+	//TODO Handle punctuation other than spaces
+	return strings.Split(strings.ToLower(rawString), " ")
+}
--- brain/markov_test.go
+++ brain/markov_test.go
@@ -2,6 +2,7 @@ package markov
 
 import (
 	"testing"
+	"reflect"
 )
 
 func TestInit(t *testing.T) {
@@ -25,13 +26,45 @@ func TestInit(t *testing.T) {
     }
 }
 
+func TestProcessString(t *testing.T) {
+	tables := []struct {
+		testcase string
+		input    string
+		expected []string
+	}{
+		{"1 word", "test", []string{"test"}},
+		{"2 words", "test data", []string{"test", "data"}},
+		{"3 words", "test data one", []string{"test", "data", "one"}},
+		{"0 words", "", []string{""}},
+		{"alphanumeric", "test1data", []string{"test1data"}},
+		{"punctuation", "test. data,", []string{"test.", "data,"}},
+		{"Capitalisation", "Test. data,", []string{"test.", "data,"}},
+		{"Mixed Case", "Test. Data,", []string{"test.", "data,"}},
+		{"AlTeRnAtInG CaSe", "TeSt. DaTa,", []string{"test.", "data,"}},
+	}
+
+	for _, table := range tables {
+		t.Logf("Testing: %s", table.testcase)
+		got := processString(table.input)
+		if !reflect.DeepEqual(got, table.expected) {
+			t.Errorf("expected: %#v, got: %#v", table.expected, got)
+		} else {
+			t.Log("Passed")
+		}
+	}
+}
+
 func TestTrain(t *testing.T) {
 	tables := []struct {
 		testcase string
 		input    string
         errors   bool
 	}{
-        {"Train on a word", "word", false},
+        {"One word", "word", false},
+        {"Two words", "two words", false},
+        {"Two words with punctuation ", "two, words", false},
+        {"Word and number", "1 one", false},
+        {"Empty string", "", false},
     }
 
     brain := new(Brain)
