610aaf5e8f6341c954deaf111176bcabace92d39
Matthew Chubb <chubb.matthew@gmail.com>
2020-09-21 21:49:28 +0100

    Copypaste markov unit tests

--- brain/markov_test.go
+++ brain/markov_test.go
@@ -3,6 +3,7 @@ package markov
 import (
 	"testing"
 	"reflect"
+	"github.com/mb-14/gomarkov"
 )
 
 func TestInit(t *testing.T) {
@@ -123,5 +124,95 @@ func TestGenerate(t *testing.T) {
         } else {
             t.Errorf("First letter %q not capitalised", got[0])
         }
+	}
+}
+
+func TestGenerateSentence(t *testing.T) {
+	/*
+	   Markov chains are inherently random, and so there's no real point in trying to test them deterministically.
+	   Instead, we can feed it various prompts and check we get _something_ out the other side
+	   TODO Check that what we get out is expected:
+	   * Contains only the words "test" and "data" in that order
+	   * Does not hit the tokensLengthLimit
+	*/
+	tables := []struct {
+		testcase string
+		input    []string
+	}{
+		{"Null", []string{}},
+		//TODO Trim empty strings from input
+		//{"Empty string", []string{""}},
+		{"1 word", []string{"test"}},
+		{"1 word 2", []string{"data"}},
+		{"2 words", []string{"test", "data"}},
+		{"3 words", []string{"test", "data", "test"}},
+		{"Unknown word", []string{"testing"}},
+	}
+
+    brain := new(Brain)
+    brain.Init(1, 32)
+
+	brain.Train("test data test data test data")
+	brain.Train("data test data test data")
+	brain.Train("test data test data test data")
+
+	for _, table := range tables {
+		t.Logf("Testing: %s", table.testcase)
+		got := brain.generateSentence(table.input)
+
+		if len(got) < 1 {
+			t.Errorf("prompt: %#v, got: %#v", table.input, got)
+		} else if got[0] == gomarkov.StartToken {
+			t.Errorf("Start token found, got: %#v", got)
+		} else if got[len(got)-1] == gomarkov.EndToken {
+			t.Errorf("End token found, got: %#v", got)
+		} else {
+			//t.Logf("Got: %#v", got)
+			t.Logf("Passed (%d tokens returned)", len(got))
+		}
+	}
+}
+
+func TestTrimMessage(t *testing.T){
+	tables := []struct {
+		testcase string
+		input    []string
+		expected []string
+	}{
+		{"1 uncommon word", []string{"test"}, []string{"test"}},
+		{"1 uncommon word, 1 common word", []string{"the", "test"}, []string{"test"}},
+		{"Mention", []string{"test", "@self"}, []string{"test"}},
+	}
+
+	for _, table := range tables {
+		t.Logf("Testing: %s", table.testcase)
+		got := trimMessage(table.input)
+		if !reflect.DeepEqual(got, table.expected) {
+			t.Errorf("expected: %#v, got: %#v", table.expected, got)
+		} else {
+			t.Log("Passed")
+		}
+	}
+}
+
+func TestIsStopWord(t *testing.T){
+	tables := []struct {
+		testcase string
+		input    string
+		expected bool
+	}{
+		{"Non stopword", "test", false},
+		{"Stopword", "the", true},
+		{"Contains stopword", "theadore", false},
+	}
+
+	for _, table := range tables {
+		t.Logf("Testing: %s", table.testcase)
+		got := isStopWord(table.input)
+		if !reflect.DeepEqual(got, table.expected) {
+			t.Errorf("expected: %#v, got: %#v", table.expected, got)
+		} else {
+			t.Log("Passed")
+		}
 	}
 }
